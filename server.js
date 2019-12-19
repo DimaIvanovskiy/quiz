b3adea05900bf8f00feee2349d6a6cce64dfbf4f
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2019-12-19 10:18:55 +0500

    new

--- server.js
+++ server.js
@@ -10,8 +10,36 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err);
     }
-
     console.log(`server is listening on ${port}`);
+});
+
+animal_questions = [
+    {
+        'question': 'Какой тигр самый крупный?',
+        'variants': ["Амурский", 'Малазийский', 'Индийский', 'Сумматранский'],
+        'answer': 'Амурский'
+    },
+    {
+        'question': 'Что говорит кошечка?',
+        'variants': ["Мяу", 'Гав', 'Кукареку', 'Хрю'],
+        'answer': 'Мяу'
+    },
+    {
+        'question': 'Что говорит собачка?',
+        'variants': ["Мяу", 'Гав', 'Кукареку', 'Хрю'],
+        'answer': 'Гав'
+    }
+];
+
+app.get('/quiz/animals', (request, response) => {
+    const to_send = [];
+    for (const item of animal_questions) {
+        to_send.push({
+            'question': item['question'],
+            'variants': item['variants']
+        })
+    }
+    response.json(to_send);
 });
 
 app.get('/orgsList', (request, response) => {
